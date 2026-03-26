559e43852851adf2344788436b646ba984edcd23
MatthewPryor1 <matthew.pryor@graffinity.io>
2026-03-26 20:26:38 +0000

    Add new plant cards with images and back text
    Added three new cards with images and descriptions.

---
+++ cards.js
@@ -0,0 +1,29 @@
+const cards = [
+  {
+    id: "card_2",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034506_4674.jpg",
+    front_image_url_2: "https://apps.rhs.org.uk/plantselectorimages/detail/moja02566.jpg",
+    front_image_url_3: "",
+    back_text: `Griselinia littoralis
+
+(New Zealand broadleaf)`
+  },
+  {
+    id: "card_3",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/elbo14556.jpg",
+    front_image_url_2: "https://apps.rhs.org.uk/plantselectorimages/detail/visi191021.jpg",
+    front_image_url_3: "",
+    back_text: `Ligustrum ovalifolium
+
+(Garden privet)`
+  },
+  {
+    id: "card_4",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0003474_788.JPG",
+    front_image_url_2: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0000553_788.JPG",
+    front_image_url_3: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0012020_788.jpg",
+    back_text: `Fatsia japonica
+
+(Japanese aralia)`
+  }
+];
