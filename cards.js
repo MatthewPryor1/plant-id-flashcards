972fb9ca4491bf7abab777d89794af5fe6be8da9
MatthewPryor1 <matthew.pryor@graffinity.io>
2026-03-27 10:40:42 +0000

    Replace card data with new plant information
    Updated card entries with new plant data and images.

--- cards.js
+++ cards.js
@@ -1,29 +1,450 @@
 const cards = [
   {
-    id: "card_2",
-    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034506_4674.jpg",
-    front_image_url_2: "https://apps.rhs.org.uk/plantselectorimages/detail/moja02566.jpg",
+    id: "card_47",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0021492_2071.jpg",
+    front_image_url_2: "",
     front_image_url_3: "",
-    back_text: `Griselinia littoralis
-
-(New Zealand broadleaf)`
+    back_text: "Abelia × grandiflora\n\n(glossy abelia)"
   },
   {
-    id: "card_3",
-    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/elbo14556.jpg",
-    front_image_url_2: "https://apps.rhs.org.uk/plantselectorimages/detail/visi191021.jpg",
+    id: "card_48",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035452_4309.jpg",
+    front_image_url_2: "",
     front_image_url_3: "",
-    back_text: `Ligustrum ovalifolium
-
-(Garden privet)`
+    back_text: "Acanthus mollis\n\n(bear's breech)"
   },
   {
-    id: "card_4",
-    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0003474_788.JPG",
-    front_image_url_2: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0000553_788.JPG",
-    front_image_url_3: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0012020_788.jpg",
-    back_text: `Fatsia japonica
-
-(Japanese aralia)`
+    id: "card_49",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/elbo68052.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Acer palmatum\n\n(Japanese maple)"
+  },
+  {
+    id: "card_50",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_RHS-0008730_49.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Achillea millefolium\n\n(common yarrow)"
+  },
+  {
+    id: "card_51",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/vaem03257.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Aegopodium podagraria\n\n(ground elder)"
+  },
+  {
+    id: "card_52",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0005811_89.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Alchemilla mollis\n\n(lady’s mantle)"
+  },
+  {
+    id: "card_53",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/elbo14864.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Armeria maritima\n\n(thrift)"
+  },
+  {
+    id: "card_54",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/visi128473.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Astrantia major 'Star of Passion'\n\n(greater masterwort)"
+  },
+  {
+    id: "card_55",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0036008_3483.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Bellis perennis\n\n(daisy)"
+  },
+  {
+    id: "card_56",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/elbo33212.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Betula pendula\n\n(silver birch)"
+  },
+  {
+    id: "card_57",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_PUB0000254_10610.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Buddleja davidii\n\n(butterfly bush)"
+  },
+  {
+    id: "card_58",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-MAR0015606_295.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Caltha palustris\n\n(marsh marigold)"
+  },
+  {
+    id: "card_59",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS-0005209.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Calystegia sepium\n\n(hedge bindweed)"
+  },
+  {
+    id: "card_60",
+    front_image_url_1: "",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Campsis radicans 'Indian Summer'\n\n(trumpet vine)"
+  },
+  {
+    id: "card_61",
+    front_image_url_1: "",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Canna indica 'Pretoria'\n\n(Indian shot)"
+  },
+  {
+    id: "card_62",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0015991_11873.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Capsella bursa-pastoris\n\n(shepherd’s purse)"
+  },
+  {
+    id: "card_63",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOS8133%5B1%5D_358.jpg",
+    front_image_url_2: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOS9280_358.jpg",
+    front_image_url_3: "",
+    back_text: "Centaurea cyanus\n\n(cornflower)"
+  },
+  {
+    id: "card_64",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0011252_4332.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Corydalis flexuosa\n\n(blue corydalis)"
+  },
+  {
+    id: "card_65",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035788_4153.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Corylus avellana\n\n(hazel)"
+  },
+  {
+    id: "card_66",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_RHS-0007064_6894.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Cosmos bipinnatus\n\n(cosmea)"
+  },
+  {
+    id: "card_67",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0005133_10215.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Cotinus coggygria\n\n(smoke tree)"
+  },
+  {
+    id: "card_68",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035898_4166.jpg",
+    front_image_url_2: "https://www.tree-guide.com/images/styles/600x450-copy_/public/common-hawthorn-flower.jpg?itok=iWncYunQ",
+    front_image_url_3: "https://www.tree-guide.com/images/styles/600x450-copy_/public/common-hawthorn-terminal-bud.jpg?itok=0GH_h5cr",
+    back_text: "Crataegus monogyna\n\n(common hawthorn)"
+  },
+  {
+    id: "card_69",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0008363_3209.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Dahlia 'Bishop of Llandaff'\n\n(dahlia ‘Bishop of Llandaff’)"
+  },
+  {
+    id: "card_70",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0004667_8395.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Daucus carota\n\n(wild carrot)"
+  },
+  {
+    id: "card_71",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/_KOS9973.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Echinacea purpurea 'Sensation Pink'\n\n(purple coneflower)"
+  },
+  {
+    id: "card_72",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/elbo13044.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Eryngium giganteum 'Miss Willmott's Ghost'\n\n(Miss Willmott’s ghost)"
+  },
+  {
+    id: "card_73",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0028964_769.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Euphorbia characias subsp. wulfenii\n\n(Mediterranean spurge)"
+  },
+  {
+    id: "card_74",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/rera04132.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Forsythia × intermedia\n\n(forsythia)"
+  },
+  {
+    id: "card_75",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-RHS_April_2018_101_814.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Fritillaria meleagris\n\n(snake’s head fritillary)"
+  },
+  {
+    id: "card_76",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035798_3951.jpg",
+    front_image_url_2: "https://www.tree-guide.com/images/styles/600x450-copy_/public/ginkgo-leaf.jpg?itok=bFc72-Q2",
+    front_image_url_3: "https://www.tree-guide.com/images/styles/600x450-copy_/public/ginkgo-axial-buds.jpg?itok=uav9xOPU",
+    back_text: "Ginkgo biloba\n\n(maidenhair tree)"
+  },
+  {
+    id: "card_77",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0030871_7072.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Heliotropium arborescens\n\n(common heliotrope)"
+  },
+  {
+    id: "card_78",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/elbo31084.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Iris pseudacorus\n\n(yellow iris)"
+  },
+  {
+    id: "card_79",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0006028_8132.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Lavandula angustifolia\n\n(English lavender)"
+  },
+  {
+    id: "card_80",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035785_4155.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Leucanthemum vulgare\n\n(ox-eye daisy)"
+  },
+  {
+    id: "card_81",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-PUB0021577_1148.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Lewisia cotyledon\n\n(Siskiyou lewisia)"
+  },
+  {
+    id: "card_82",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0007338_3712.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Lobelia cardinalis\n\n(cardinal flower)"
+  },
+  {
+    id: "card_83",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0010184_3895.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Lonicera periclymenum 'Graham Thomas'\n\n(common honeysuckle)"
+  },
+  {
+    id: "card_84",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034273_4813.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Magnolia stellata\n\n(star magnolia)"
+  },
+  {
+    id: "card_85",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034554_8807.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Matteuccia struthiopteris\n\n(shuttlecock fern)"
+  },
+  {
+    id: "card_86",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0026463_3935.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Metasequoia glyptostroboides\n\n(dawn redwood)"
+  },
+  {
+    id: "card_87",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/phbe08319.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Miscanthus sinensis 'Morning Light'\n\n(eulalia)"
+  },
+  {
+    id: "card_88",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_PUB0029252_8340.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Nigella damascena\n\n(love-in-a-mist)"
+  },
+  {
+    id: "card_89",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-MAR0053181_1346.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Oenothera biennis\n\n(common evening primrose)"
+  },
+  {
+    id: "card_90",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035917_3890.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Parthenocissus quinquefolia\n\n(Virginia creeper)"
+  },
+  {
+    id: "card_91",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034409_4646.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Phlomis fruticosa\n\n(Jerusalem sage)"
+  },
+  {
+    id: "card_92",
+    front_image_url_1: "",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Phlox paniculata 'Olympus'\n\n(perennial phlox)"
+  },
+  {
+    id: "card_93",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0034360_3354.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Pontederia cordata\n\n(pickerel weed)"
+  },
+  {
+    id: "card_94",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_MAR0015575_1527.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Primula vulgaris\n\n(primrose)"
+  },
+  {
+    id: "card_95",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/sepa02104.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Prunella vulgaris\n\n(selfheal)"
+  },
+  {
+    id: "card_96",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-NHDE18096_1572.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Pulsatilla vulgaris\n\n(pasqueflower)"
+  },
+  {
+    id: "card_97",
+    front_image_url_1: "",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Ranunculus repens\n\n(creeping buttercup)"
+  },
+  {
+    id: "card_98",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0014606_1601.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Rhinanthus minor\n\n(yellow rattle)"
+  },
+  {
+    id: "card_99",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0026549_5929.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Rudbeckia fulgida var. deamii\n\n(Deam’s coneflower)"
+  },
+  {
+    id: "card_100",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOT1102_1760.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Salvia nemorosa 'Ostfriesland'\n\n(Balkan clary)"
+  },
+  {
+    id: "card_101",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_WSYD0012466_11855.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Stellaria media\n\n(common chickweed)"
+  },
+  {
+    id: "card_102",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/RHS_RHS-0008642_1882.JPG",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Stipa gigantea\n\n(golden oats)"
+  },
+  {
+    id: "card_103",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0012099_5374.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Symphyotrichum novi-belgii 'Fellowship'\n\n(New York aster)"
+  },
+  {
+    id: "card_104",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOT2588_13390.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Tagetes erecta\n\n(African marigold)"
+  },
+  {
+    id: "card_105",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/eyma01394.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Taraxacum officinale\n\n(dandelion)"
+  },
+  {
+    id: "card_106",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/moja09014.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Trachelospermum jasminoides\n\n(star jasmine)"
+  },
+  {
+    id: "card_107",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0035821_4099.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Typha angustifolia\n\n(lesser bulrush)"
+  },
+  {
+    id: "card_108",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/sepa04809.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Urtica dioica\n\n(common stinging nettle)"
+  },
+  {
+    id: "card_109",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/MAR0003574_1993.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Verbena bonariensis\n\n(purple top)"
+  },
+  {
+    id: "card_110",
+    front_image_url_1: "https://apps.rhs.org.uk/plantselectorimages/detail/WSY0040456_12943.jpg",
+    front_image_url_2: "",
+    front_image_url_3: "",
+    back_text: "Wisteria sinensis\n\n(Chinese wisteria)"
   }
 ];
